3f615e08192be7b993f2dcedcef1341c120746ee
Duncantse <tse03011950@gmail.com>
2026-09-03 09:07:50 +0000

    lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -5,5 +5,5 @@ whole_cucumbers_per_person = cucumbers // num_people
 
 print(whole_cucumbers_per_person)
 
-whole_cucumbers_per_person = cucumbers / num_people
-print(whole_cucumbers_per_person)
+float_cucumbers_per_person = cucumbers / num_people
+print(float_cucumbers_per_person)
