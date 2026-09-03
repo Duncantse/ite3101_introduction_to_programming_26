86233d9b35d9d4509315e9e772f8f0782ef39266
Duncantse <tse03011950@gmail.com>
2026-09-03 14:17:38 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -1,6 +1,6 @@
 # Complete the if and elif statements!
 def grade_converter(grade: int) -> str:
-    if grade_converter > 90:
+    if grade_converter() > 90:
         return "A"
     elif grade_converter >=80 :
         return "B"
