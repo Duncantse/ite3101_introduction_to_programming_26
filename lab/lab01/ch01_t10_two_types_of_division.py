87a543500bbe7a3563980731bbf1defd68cfe9ad
Duncantse <tse03011950@gmail.com>
2026-09-03 09:05:45 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,8 +1,8 @@
 cucumbers = 100
 num_people = 6
 
-whole_cucumbers_per_person += cucumbers // num_people
+whole_cucumbers_per_person = cucumbers // num_people
 
 print(whole_cucumbers_per_person)
 
-whole_cucumbers_per_person
+whole_cucumbers_per_person =
