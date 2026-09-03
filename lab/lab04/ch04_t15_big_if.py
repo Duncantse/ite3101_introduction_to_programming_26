541e120d8b54ddb8e507e6e85318954f113be46e
Duncantse <tse03011950@gmail.com>
2026-09-03 14:22:15 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -2,7 +2,7 @@
 def grade_converter(grade: int) -> str:
     if grade_converter > 90:
         return "A"
-    elif grade_converter >= 80:
+    elif grade_converter() >= 80:
         return "B"
     elif grade_converter >= 70:
         return "C"
