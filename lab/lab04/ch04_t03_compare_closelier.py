3600ca095a65731ff5fb2bc023c3e85e410a7710
Duncantse <tse03011950@gmail.com>
2026-09-03 14:02:38 +0000

    [skip actions] lab/lab04/ch04_t03_compare_closelier.py

--- lab/lab04/ch04_t03_compare_closelier.py
+++ lab/lab04/ch04_t03_compare_closelier.py
@@ -3,17 +3,9 @@
 # (20 - 10) > 15
 bool_one = False  # We did this one for you!
 
-bool_two = None
+
 # (10 + 17) == 3**16
 # Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
-if (10 + 17) == 3**16:
-    {
-        bool_two = True 
-    }  
-    else
-    {
-        bool_two = False 
-    }
 
 
 
