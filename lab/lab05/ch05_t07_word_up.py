d3e077644114ca0df512887356d888cdce9456bd
Duncantse <tse03011950@gmail.com>
2026-09-10 08:12:57 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -2,10 +2,9 @@ pyg = 'ay'
 
 original = input('Enter a word:')
 word = original.lower()
+first = word[0]
 
 if len(original) > 0 and original.isalpha():
     print(original)
 else:
     print('empty')
-
-first = word[0]
