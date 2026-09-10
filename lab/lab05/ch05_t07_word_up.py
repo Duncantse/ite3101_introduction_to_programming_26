000544100cf9b9711b8c5c9b7620cc767223bf66
Duncantse <tse03011950@gmail.com>
2026-09-10 08:05:37 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -1,7 +1,7 @@
 pyg = 'ay'
 
 original = input('Enter a word:')
-word = original.lower()
+word = original.lower()-case
 first = word[0]
 if len(original) > 0 and original.isalpha():
     print(original)
