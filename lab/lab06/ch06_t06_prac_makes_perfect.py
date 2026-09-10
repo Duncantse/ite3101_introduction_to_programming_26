c78ef736035d6d3be416ab903bffe88a3bc7eec1
Duncantse <tse03011950@gmail.com>
2026-09-10 08:46:38 +0000

    lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,9 +1,9 @@
 def cube(number):
- return(number * number * number)
+    return (number * number * number)
+
 
 def by_three(number):
- if number % 3 == 0 :
-   number = number % 3
-   return number
- else:
-  return False
+    if number % 3 == 0:
+        return cube(number)
+    else:
+        return False
