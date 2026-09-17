adf55f023fb006cd382e49d64bdaadf9714ad9a3
Duncantse <tse03011950@gmail.com>
2026-09-17 08:01:19 +0000

    [skip actions] lab/lab06/ch06_t19_builtin_functions.py

--- lab/lab06/ch06_t19_builtin_functions.py
+++ lab/lab06/ch06_t19_builtin_functions.py
@@ -1,5 +1,4 @@
 def distance_from_zero(num):
-    return type(num) == int or float:
-if type(num) == int or float:
-    return abs(num)
-return"Nope"
+    if type(num) == int or float:
+        return abs(num)
+    return "Nope"
