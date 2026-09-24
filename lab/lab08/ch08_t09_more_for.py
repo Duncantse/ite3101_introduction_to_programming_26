1c92217d574472b07d1d7ac52da12ad9e9c67531
Duncantse <tse03011950@gmail.com>
2026-09-24 08:29:07 +0000

    [skip actions] lab/lab08/ch08_t09_more_for.py

--- lab/lab08/ch08_t09_more_for.py
+++ lab/lab08/ch08_t09_more_for.py
@@ -2,7 +2,7 @@ start_list = [5, 3, 1, 2, 4]
 square_list = []
 
 # Your code here!
-for number in square_list:
-    number = start_list * 2
+for number in start_list:
+    number = number * 2
 square_list.sort()
 print(square_list)
