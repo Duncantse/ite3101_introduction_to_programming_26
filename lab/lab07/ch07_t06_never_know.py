3e2f58dacd6b8592d40f6018637df9c9d2e3c216
Duncantse <tse03011950@gmail.com>
2026-09-24 07:27:03 +0000

    lab/lab07/ch07_t06_never_know.py

--- lab/lab07/ch07_t06_never_know.py
+++ lab/lab07/ch07_t06_never_know.py
@@ -23,5 +23,5 @@ def rental_car_cost(days: int) -> int:
 
 
 
-def trip_cost(city: str, days: int,  spend: str) -> int:
+def trip_cost(city: str, days: int,  spending_money: str) -> int:
     return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) +  spending_money
