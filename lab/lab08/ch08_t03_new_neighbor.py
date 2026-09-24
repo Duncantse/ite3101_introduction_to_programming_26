88968f010b96f139a50a5a0c89337eacc3ba5647
Duncantse <tse03011950@gmail.com>
2026-09-24 07:32:36 +0000

    [skip actions] lab/lab08/ch08_t03_new_neighbor.py

--- lab/lab08/ch08_t03_new_neighbor.py
+++ lab/lab08/ch08_t03_new_neighbor.py
@@ -6,4 +6,4 @@ zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"]
 zoo_animals[2] = "hyena"
 
 # What shall fill the void left by our dear departed tiger?
-# Your code here!
+zoo_animals[3] = "tiger"
