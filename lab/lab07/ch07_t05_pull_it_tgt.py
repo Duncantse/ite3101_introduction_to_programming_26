c59b7d7539790cbafdece613bd4dc900256aa327
Duncantse <tse03011950@gmail.com>
2026-09-24 07:16:40 +0000

    lab/lab07/ch07_t05_pull_it_tgt.py

--- lab/lab07/ch07_t05_pull_it_tgt.py
+++ lab/lab07/ch07_t05_pull_it_tgt.py
@@ -20,3 +20,7 @@ def rental_car_cost(days: int) -> int:
     elif days >= 3:
         cost -= 20
     return cost
+
+
+def trip_cost(city: str, days: int) -> int:
+    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
