69d840758c3f6a4b6fd3f40c98e6e042ff375e57
Duncantse <tse03011950@gmail.com>
2026-09-24 08:05:24 +0000

    [skip actions] lab/lab08/ch08_t07_maintaining_order.py

--- lab/lab08/ch08_t07_maintaining_order.py
+++ lab/lab08/ch08_t07_maintaining_order.py
@@ -1,5 +1,5 @@
 animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
-duck_index = None  # Use index() to find "duck"
+duck_index = animals[2]  # Use index() to find "duck"
 
 # Your code here!
 
